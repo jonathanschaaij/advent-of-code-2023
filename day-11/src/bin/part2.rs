19f263e26e6aad390ce90ad327d054c8f18ef37c
Jonathan Schaaij <jonathan.schaaij@gmail.com>
2023-12-11 20:12:24 +0100

    Day 11 - minor speed improvements

--- day-11/src/bin/part2.rs
+++ day-11/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, time::Instant};
+use std::{collections::HashSet, task::Wake, time::Instant};
 
 const EXPANSION: i64 = 1_000_000;
 
@@ -38,7 +38,7 @@ fn solve(file: &str) -> i64 {
         }
     }
 
-    let galaxies: Vec<(i64, i64)> = space
+    let mut galaxies: Vec<(i64, i64)> = space
         .iter()
         .enumerate()
         .flat_map(|(y, row)| {
@@ -50,18 +50,20 @@ fn solve(file: &str) -> i64 {
         .collect();
 
     // Move galaxies
-    let mut moved_galaxies = Vec::new();
-    for (x, y) in galaxies.iter() {
-        let newx = x + empty_cols.iter().filter(|&&c| c < *x).count() as i64 * (EXPANSION - 1);
-        let newy = y + empty_rows.iter().filter(|&&r| r < *y).count() as i64 * (EXPANSION - 1);
-        moved_galaxies.push((newx, newy));
+    // let mut moved_galaxies = Vec::new();
+    for (x, y) in galaxies.iter_mut() {
+        let newx = *x + empty_cols.iter().filter(|&&c| c < *x).count() as i64 * (EXPANSION - 1);
+        let newy = *y + empty_rows.iter().filter(|&&r| r < *y).count() as i64 * (EXPANSION - 1);
+        // moved_galaxies.push((newx, newy));
+        *x = newx;
+        *y = newy;
     }
 
     let mut total_dist = 0;
-    for i in 0..(moved_galaxies.len() - 1) {
-        for j in (i + 1)..moved_galaxies.len() {
-            let (x1, y1) = moved_galaxies[i];
-            let (x2, y2) = moved_galaxies[j];
+    for i in 0..(galaxies.len() - 1) {
+        for j in (i + 1)..galaxies.len() {
+            let (x1, y1) = galaxies[i];
+            let (x2, y2) = galaxies[j];
 
             // manhattan distance
             let dist = (x1 as i64 - x2 as i64).abs() + (y1 as i64 - y2 as i64).abs();
